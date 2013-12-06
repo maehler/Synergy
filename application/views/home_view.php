4a20fc45f4c32bff600a35d1ff56b15bfbd28447
Niklas Mähler <niklas.mahler@umb.no>
2013-12-06 10:59:30 +0100

    added start page, no header or footer so far

---
+++ application/views/home_view.php
@@ -0,0 +1,70 @@
+<article>
+    <header>
+        <h2>Welcome to $title</h2>
+    </header>
+    <aside class="box small float-right" style="width:200px;">
+        <span class="bold">syn&bull;er&bull;gy</span> [ˈsinərjē]<br />
+        the interaction or cooperation of two or more organizations,
+        substances, or other agents to produce a combined effect greater
+        than the sum of their separate effects.
+    </aside>
+    <p>
+        This is a set of tools for exploring the gene regulation in the
+        cyanobacterium <em>Synechocystis</em>.
+    </p>
+    <p>
+        Right now, the site is password-protected and under development.
+        Therefore, full functionality cannot be guaranteed. The
+        development will continue during this summer.
+    </p>
+
+    <button id="quickstart" class="big-fancy center-div">
+        Start exploring <em>Syn</em>ergy
+    </button>
+
+    <p>
+        Many of the tools on this site rely on HTML5 and JavaScript.
+        Therefore I recommend that you use one of the following
+        browsers:
+    </p>
+    <ul>
+        <li><a class="external" href="https://www.google.com/intl/en/chrome/browser/">
+                Google Chrome</a> (v19+)
+        </li>
+        <li><a class="external" href="http://www.mozilla.org/en-US/firefox/new/">
+                Firefox</a> (v12+)</li>
+        <li><a class="external" href="http://www.apple.com/safari/">Safari</a> (v5+)</li>
+    </ul>
+    <p>
+        Functionality has been tested in all of the above browsers, but
+        most of the development has been done in Google Chrome and
+        Firefox, so any of those would be a good option. Also, make sure
+        that JavaScript is enabled. Make sure pop-ups are allowed, as
+        well as some links trigger this. I won't spam you, I promise.
+    </p>
+    <p>
+        Internet Explorer 9 has been tested, and the basic functionality
+        is ok, but some things are rendered very weird, so I wouldn't
+        recommend it.
+    </p>
+    <section>
+        <header><h3>Something not working?</h3></header>
+        <p>
+            If you find something that is not working as expected, or
+            you come up with some feature you'd like to have, feel free
+            to contact me. You can reach me at <a
+            href="mailto:niklas.mahler@umb.no">niklas.mahler@umb.no</a>.
+            It could be anything, ranging from how data is presented to
+            colors of buttons.
+        </p>
+        <p>
+            Currently there is a <a
+            href="http://en.wikipedia.org/wiki/Git_%28software%29"
+            class="external" target="_blank">git</a> repository available at <a
+            href="https://bitbucket.org/mcfjodor/synergy/overview"
+            target="_blank" class="external">Bitbucket</a>. At the moment it is private,
+            but an issue tracker and/or a wiki will become public soon
+            enough.
+        </p>
+    </section>
+</article>
