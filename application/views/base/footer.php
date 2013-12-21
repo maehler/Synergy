f0603d73349250fc85c5b6eef738ce575fc406da
Niklas Mähler <niklas.mahler@umb.no>
2013-12-21 10:26:16 +0100

    added jquery to default footer

--- application/views/base/footer.php
+++ application/views/base/footer.php
@@ -9,14 +9,15 @@
         <section id="noscript-warn"><em>Syn</em>ergy works best with
         JavaScript enabled</section>
     </noscript>
+    <script type="text/javascript" src="<?php echo base_url(array('assets', 'js', 'jquery-1.10.2.min.js')); ?>"></script>
     <?php if (isset($scripts) && $scripts !== NULL): ?>
-   	<?php foreach ($scripts as $js): ?>
-   	<script type="text/javascript" src="<?php echo $js ?>"></script>
-   	<?php endforeach ?>
+    <?php foreach ($scripts as $js): ?>
+    <script type="text/javascript" src="<?php echo $js ?>"></script> 
+    <?php endforeach ?>
     <?php endif ?>
     <?php if (isset($inlinejs) && !empty($inlinejs)): ?>
     <script type="text/javascript">
-    	<?php echo $inlinejs ?>
+    <?php echo $inlinejs ?>
     </script>
     <?php endif ?>
 </body>
