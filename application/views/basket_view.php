381907b73a11e34f45adaf5ed78e6aaa51b613c3
Niklas Mähler <niklas.mahler@umb.no>
2014-01-23 12:06:21 -0800

    skeleton for go and motif enrichment

--- application/views/basket_view.php
+++ application/views/basket_view.php
@@ -4,36 +4,110 @@
 <p>You have no genes in your basket. This is a good time to <a href="<?php echo base_url('search'); ?>">go fetch some</a>!</p>
 <?php else: ?>
 <section>
-	<button id="empty-basket">Empty basket</button>
+	<table id="basket-table" class="datatable small">
+		<thead>
+			<th>ORF</th>
+			<th>Accession</th>
+			<th>Symbol</th>
+			<th>Category</th>
+			<th>Definition</th>
+			<th>Regulatory function</th>
+		</thead>
+		<tbody>
+		<?php foreach ($genes as $gene): ?>
+			<tr>
+				<td><a href="<?php echo base_url(array('gene', 'details', $gene['orf_id'])); ?>"><?php echo $gene['orf_id'] ?></a></td>
+				<td><?php echo $gene['refseq_id'] ?></td>
+				<td><em><?php echo $gene['symbol'] ?></em></td>
+				<td><?php echo $gene['category'] ?></td>
+				<td><?php echo $gene['definition'] ?></td>
+				<td><?php echo $gene['tf'] == 1 ? "Yes" : "No" ?></td>
+			</tr>
+		<?php endforeach ?>
+		</tbody>
+		<tfoot>
+			<th>ORF</th>
+			<th>Accession</th>
+			<th>Symbol</th>
+			<th>Category</th>
+			<th>Definition</th>
+			<th>Regulatory function</th>
+		</tfoot>
+	</table>
+	<div class="clear-fix small">
+		<button id="empty-basket">Empty basket</button>
+	</div>
 </section>
-<table id="basket-table" class="datatable small">
-	<thead>
-		<th>ORF</th>
-		<th>Accession</th>
-		<th>Symbol</th>
-		<th>Category</th>
-		<th>Definition</th>
-		<th>Regulatory function</th>
-	</thead>
-	<tbody>
-	<?php foreach ($genes as $gene): ?>
-		<tr>
-			<td><a href="<?php echo base_url(array('gene', 'details', $gene['orf_id'])); ?>"><?php echo $gene['orf_id'] ?></a></td>
-			<td><?php echo $gene['refseq_id'] ?></td>
-			<td><em><?php echo $gene['symbol'] ?></em></td>
-			<td><?php echo $gene['category'] ?></td>
-			<td><?php echo $gene['definition'] ?></td>
-			<td><?php echo $gene['tf'] == 1 ? "Yes" : "No" ?></td>
-		</tr>
-	<?php endforeach ?>
-	</tbody>
-	<tfoot>
-		<th>ORF</th>
-		<th>Accession</th>
-		<th>Symbol</th>
-		<th>Category</th>
-		<th>Definition</th>
-		<th>Regulatory function</th>
-	</tfoot>
-</table>
+
+<section>
+	<h3>Enrichment tools</h3>
+	<label for="enrichment-go-radio">GO enrichment</label>
+	<input id="enrichment-go-radio" name="enrichment-radio" type="radio" value="go-pane" checked>
+	<label for="enrichment-motif-radio">Motif enrichment</label>
+	<input id="enrichment-motif-radio" name="enrichment-radio" type="radio" value="motif-pane">
+
+	<section id="go-pane" class="small">
+		<label for="go-p-th">FDR threshold</label>
+		<select id="go-p-th">
+			<option value="0.01">0.01</option>
+			<option value="0.05" selected>0.05</option>
+			<option value="0.1">0.1</option>
+		</select><br>
+		<button id="start-go-enrichment">Calculate</button>
+
+		<table id="go-table">
+			<thead>
+				<tr>
+					<th>GO term</th>
+					<th>Category</th>
+					<th>Description</th>
+					<th>FDR</th>
+				</tr>
+			</thead>
+			<tbody>
+			</tbody>
+			<tfoot>
+				<tr>
+					<th>GO term</th>
+					<th>Category</th>
+					<th>Description</th>
+					<th>FDR</th>
+				</tr>
+			</tfoot>
+		</table>
+	</section>
+
+	<section id="motif-pane" class="hidden small">
+		<label for="motif-p-th">FDR threshold</label>
+		<select id="motif-p-th">
+			<option value="0.01">0.01</option>
+			<option value="0.05" selected>0.05</option>
+			<option value="0.1">0.1</option>
+		</select><br>
+		<label for="central-motifs">Central motifs</label>
+		<input id="central-motifs" type="checkbox" checked><br>
+		<button id="start-motif-enrichment">Calculate</button>
+
+		<table id="motif-table">
+			<thead>
+				<tr>
+					<th>Motif</th>
+					<th>Length</th>
+					<th>Description</th>
+					<th>FDR</th>
+				</tr>
+			</thead>
+			<tbody>
+			</tbody>
+			<tfoot>
+				<tr>
+					<th>GO term</th>
+					<th>Category</th>
+					<th>Description</th>
+					<th>FDR</th>
+				</tr>
+			</tfoot>
+		</table>
+	</section>
+</section>
 <?php endif ?>
--- assets/js/basket.js
+++ assets/js/basket.js
@@ -1,15 +1,45 @@
 "use strict";
 
+function emptyBasket() {
+	$.ajax({
+		url: 'api/empty_basket',
+		type: 'GET',
+		success: function () {
+			location.reload();
+		}
+	});
+}
+
+function enrichmentToggle() {
+	var panes = ['go-pane', 'motif-pane'];
+	for (var i = 0; i < panes.length; i++) {
+		if ($(this).val() == panes[i]) {
+			$('#'+panes[i]).removeClass('hidden');
+		} else {
+			$('#'+panes[i]).addClass('hidden');
+		}
+	}
+}
+
+function goEnrichment() {
+
+}
+
+function motifEnrichment() {
+	
+}
+
 $(function () {
 	$('#basket-table').dataTable();
 
-	$('#empty-basket').click(function () {
-		$.ajax({
-			url: 'api/empty_basket',
-			type: 'GET',
-			success: function () {
-				location.reload();
-			}
-		});
-	});
+	// Table button listeners
+	$('#empty-basket').click(emptyBasket);
+
+	// Enrichment
+	$('[name="enrichment-radio"]').change(enrichmentToggle);
+	$('#go-table').dataTable();
+	$('#motif-table').dataTable();
+
+	$('#start-go-enrichment').click(goEnrichment);
+	$('#start-motif-enrichment').click(motifEnrichment);
 });
