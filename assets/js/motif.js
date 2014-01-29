9b8f7110bfd1d90ff4f153e02258bf5cdfe38783
Niklas Mähler <niklas.mahler@umb.no>
2014-01-29 15:12:14 -0800

    adjust motif logo width according to motif length

--- assets/js/motif.js
+++ assets/js/motif.js
@@ -1,13 +1,16 @@
 "use strict";
 
 $(function () {
+	var motifLen = pspm.length;
+	var canvasWidth = 50 * motifLen;
+
 	isblogo.makeLogo('motif-logo', {
 		alphabet: ['A', 'C', 'G', 'T'],
 		values: pspm
 	},
 	{
-		width: 500,
-		height: 300,
+		width: canvasWidth,
+		height: 200,
 		glyphStyle: 'bold 20pt Helvetica'
 	});
 });
