ecb9234e869b00cb3f03cfad12cceefc6d822313
Niklas Mähler <niklas.mahler@umb.no>
2014-02-03 15:00:20 -0800

    parentheses around where statement

--- application/models/genelist_model.php
+++ application/models/genelist_model.php
@@ -21,13 +21,17 @@ class Genelist_model extends CI_Model {
 		// Filtering
 		$sSearch = $this->input->post('sSearch');
 		if ($sSearch && $sSearch != '') {
+			$filterWhere = '';
 			for ($i = 0; $i < count($taColumns); $i++) {
 				if ($i == 0) {
-					$this->db->like($taColumns[$i], $sSearch);
+					$filterWhere .= "(" . $taColumns[$i] . " LIKE " . "'%" . $sSearch . "%' ";
+				} else if ($i == (count($taColumns) - 1)) {
+					$filterWhere .= "OR " . $taColumns[$i] . " LIKE " . "'%" . $sSearch . "%')";
 				} else {
-					$this->db->or_like($taColumns[$i], $sSearch);
+					$filterWhere .= "OR " . $taColumns[$i] . " LIKE " . "'%" . $sSearch . "%' ";
 				}
 			}
+			$this->db->where($filterWhere);
 		}
 
 		// Ordering
