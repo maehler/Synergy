38cc1e8b648100ae1e826d1038b26664356a297f
Niklas Mähler <niklas.mahler@umb.no>
2014-02-06 18:47:06 -0800

    temporarily enable error messages on production

--- index.php
+++ index.php
@@ -75,7 +75,7 @@ switch (ENVIRONMENT)
 	case 'testing':
 	case 'production':
 		error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT);
-		ini_set('display_errors', 0);
+		ini_set('display_errors', 1);
 	break;
 
 	default:
