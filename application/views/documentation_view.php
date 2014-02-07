baa488adbf94e8498c034d580ea8c6cd1a5cceba
Niklas Mähler <niklas.mahler@umb.no>
2014-02-07 12:36:18 -0800

    documentation

--- application/views/documentation_view.php
+++ application/views/documentation_view.php
@@ -9,7 +9,13 @@
 				<li><a href="#general-cookies">Cookies</a></li>
 			</ul>
 		</li>
-		<li><a href="#search">Search</a></li>
+		<li>
+			<a href="#search">Search</a>
+			<ul>
+				<li><a href="#search-upload">Upload a gene list</a></li>
+				<li><a href="#search-genelists">Gene lists</a></li>
+			</ul>
+		</li>
 		<li><a href="#basket">Basket</a></li>
 		<li><a href="#network">Network</a></li>
 		<li><a href="#regtools">Regulation tools</a></li>
@@ -31,10 +37,11 @@
 		the JavaScript library <a href="http://modernizr.com/">Modernizr</a> 
 		to detect what features your browser supports. If support for an 
 		essential feature cannot be detected, the site will inform you to 
-		update your browser:
+		update your browser.
 		</p>
 		<figure class="right">
-			<img src="<?php echo base_url(array('assets', 'img', 'old_browser_warn.png')) ?>" alt="Incompatible browser warning">
+			<img src="<?php echo base_url(array('assets', 'img',
+				'old_browser_warn.png')) ?>" alt="Incompatible browser warning">
 			<figcaption>
 				<p>Message shown if your browser is not supported.</p>
 			</figcaption>
@@ -60,6 +67,40 @@
 
 <section id="search">
 	<h3>Search</h3>
+	<p>In the <a href="<?php echo base_url('search'); ?>">Gene search</a>, you can search
+	for genes in <i>Synechocystis</i> that you are particulary interested in. In the
+	first table, you can do free text searches for ORF names and annotations. Whenever
+	something is entered in the search field, the table is filtered on the fly.
+	To add genes to your <a href="#basket">gene basket</a>, just check the checkbox in
+	the leftmost column. If you want to select all genes in the filtered table, just
+	click the "Select all" button. Your gene basket is updated automatically whenever
+	your selection changes.
+	</p>
+
+	<section id="search-upload">
+		<h4>Upload a gene list</h4>
+		<p>You can also upload a file with genes and load that into your gene basket.
+		When the file is parsed, only the first word in each line is taken into account.
+		For example, consider the following example:
+		</p>
+		<pre>gene1 gene2
+gene3 gene4
+gene5</pre>
+		<p>In this case, only <code>gene1</code>, <code>gene3</code> and <code>gene5</code>
+		will be searched for, the others will be ignored.
+		</p>
+	</section>
+
+	<section id="search-genelists">
+		<h4>Gene lists</h4>
+		<p>Another, more explorative, way of finding genes of interest is to take a 
+		look at the precompiled gene lists that are available. Four different categories
+		of gene lists are available; genes associated to Gene Ontology terms, genes
+		associated to motifs, genes in co-expression network clusters and genes in the
+		co-expression neighborhood of regulatory genes. By clicking the "Load"
+		button in the last column, the genes will be added to the gene basket.
+		</p>
+	</section>
 </section>
 
 <section id="basket">
