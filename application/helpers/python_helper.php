9dbd9c3139d4487277bc541d41c28b5bbe3d2eb0
Niklas Mähler <niklas.mahler@umb.no>
2014-02-11 11:43:54 -0800

    added new python runner that can use stdin

--- application/helpers/python_helper.php
+++ application/helpers/python_helper.php
@@ -30,3 +30,49 @@ if (!function_exists('run_python')) {
         return $data;
     }
 }
+
+if (!function_exists('run_python_stdinpipe')) {
+    function run_python_stdinpipe($script, $input, $params=array(), $implode_char=" ") {
+        $clean_params = array();
+        foreach ($params as $p) {
+            if (empty($p)) {
+                continue;
+            }
+            if (is_array($p)) {
+                foreach ($p as $np) {
+                    $clean_params[] = escapeshellarg($np);
+                }
+            } else {
+                $clean_params[] = escapeshellarg($p);
+            }
+        }
+        $str_params = implode($implode_char, $clean_params);
+
+        $cmd = PYTHON . ' ' . PYTHON_SCRIPTS . $script . ' ' . $str_params;
+
+        $descriptorspec = array(
+            0 => array('pipe', 'r'),
+            1 => array('pipe', 'w'),
+            2 => array('pipe', 'w')
+        );
+
+        $process = proc_open($cmd, $descriptorspec, $pipes);
+
+        $output = "";
+
+        if (is_resource($process)) {
+            fwrite($pipes[0], $input);
+            fclose($pipes[0]);
+
+            $output = stream_get_contents($pipes[1]);
+            fclose($pipes[1]);
+
+            echo stream_get_contents($pipes[2]);
+            fclose($pipes[2]);
+
+            $return_value = proc_close($process);
+        }
+
+        return $output;
+    }
+}
