16ed508f4bc441f720a7c044858c828a11485d48
Niklas Mähler <niklas.mahler@umb.no>
2014-02-25 15:29:19 -0800

    conform to my form standard

--- application/views/enrichment_view.php
+++ application/views/enrichment_view.php
@@ -6,37 +6,30 @@
 	<input id="enrichment-motif-radio" name="enrichment-radio" type="radio" value="motif-pane">
 
 	<section id="go-pane" class="small">
-		<table>
-			<tr>
-				<td class="align right">
-					<label for="go-p-th">FDR threshold</label>
-				</td>
-				<td>
-					<select id="go-p-th">
-						<option value="0.01">0.01</option>
-						<option value="0.05" selected>0.05</option>
-						<option value="0.1">0.1</option>
-						<option value="0.5">0.5</option>
-					</select>
-				</td>
-			</tr>
-			<tr>
-				<td></td>
-				<td><button id="start-go-enrichment">Calculate</button></td>				
-			</tr>
-			<tr>
-				<td class="align-right">
-					<label for="go-category-filter">GO category filter</label>
-				</td>
-				<td>
-					<select id="go-category-filter">
-						<option value="Biological process" selected>Biological process</option>
-						<option value="Molecular function">Molecular function</option>
-						<option value="Cellular component">Cellular component</option>
-					</select>
-				</td>
-			</tr>
-		</table>
+		<form>
+			<fieldset>
+				<ol>
+					<li>
+						<label for="go-p-th">FDR threshold</label>
+						<select id="go-p-th">
+							<option value="0.01">&lt;0.01</option>
+							<option value="0.05" selected>&lt;0.05</option>
+							<option value="0.1">&lt;0.10</option>
+							<option value="0.5">&lt;0.50</option>
+						</select>
+					</li>
+					<li>
+						<label for="go-category-filter">GO category filter</label>
+						<select id="go-category-filter">
+							<option value="Biological process" selected>Biological process</option>
+							<option value="Molecular function">Molecular function</option>
+							<option value="Cellular component">Cellular component</option>
+						</select>
+					</li>
+				</ol>
+			</fieldset>
+			<button type="button" id="start-go-enrichment">Calculate</button>
+		</form>
 
 		<table id="go-table" class="dataTable">
 			<thead>
@@ -61,52 +54,41 @@
 	</section>
 
 	<section id="motif-pane" class="hidden small">
-		<table>
-			<tr>
-				<td class="align-right"><label for="motif-p-th">FDR threshold</label></td>
-				<td>
-					<select id="motif-p-th">
-						<option value="0.01">&lt;0.01</option>
-						<option value="0.05" selected>&lt;0.05</option>
-						<option value="0.1">&lt;0.10</option>
-						<option value="0.2">&lt;0.20</option>
-						<option value="0.3">&lt;0.30</option>
-						<option value="0.4">&lt;0.40</option>
-						<option value="0.5">&lt;0.50</option>
-					</select>
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<label for="motif-q-th">FIMO q-value threshold</label>
-				</td>
-				<td>
-					<select id="motif-q-th">
-						<option value="0.01">&lt;0.01</option>
-						<option value="0.05">&lt;0.05</option>
-						<option value="0.10">&lt;0.10</option>
-						<option value="0.15" selected>&lt;0.15</option>
-						<option value="0.20">&lt;0.20</option>
-						<option value="0.25">&lt;0.25</option>
-						<option value="0.30">&lt;0.30</option>
-					</select>
-				</td>
-			</tr>
-			<tr>
-				<td class="align-right">
-					<label for="central-motifs">Central motifs</label>
-				</td>
-				<td>
-					<input id="central-motifs" type="checkbox" checked>
-				</td>
-			</tr>
-			<tr>
-				<td></td>
-				<td>
-					<button id="start-motif-enrichment">Calculate</button>
-				</td>
-			</tr>
-		</table>
+		<form>
+			<fieldset>
+				<ol>
+					<li>
+						<label for="motif-p-th">FDR threshold</label>
+						<select id="motif-p-th">
+							<option value="0.01">&lt;0.01</option>
+							<option value="0.05" selected>&lt;0.05</option>
+							<option value="0.1">&lt;0.10</option>
+							<option value="0.2">&lt;0.20</option>
+							<option value="0.3">&lt;0.30</option>
+							<option value="0.4">&lt;0.40</option>
+							<option value="0.5">&lt;0.50</option>
+						</select>
+					</li>
+					<li>
+						<label for="motif-q-th">FIMO q-value threshold</label>
+						<select id="motif-q-th">
+							<option value="0.01">&lt;0.01</option>
+							<option value="0.05">&lt;0.05</option>
+							<option value="0.10">&lt;0.10</option>
+							<option value="0.15" selected>&lt;0.15</option>
+							<option value="0.20">&lt;0.20</option>
+							<option value="0.25">&lt;0.25</option>
+							<option value="0.30">&lt;0.30</option>
+						</select>
+					</li>
+					<li>
+						<label for="central-motifs">Central motifs</label>
+						<input id="central-motifs" type="checkbox" checked>
+					</li>
+				</ol>
+			</fieldset>
+			<button type="button" id="start-motif-enrichment">Calculate</button>
+		</form>
 
 		<table id="motif-table" class="dataTable">
 			<thead>
