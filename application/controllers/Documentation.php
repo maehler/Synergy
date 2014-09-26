5bba8f84bd5d577b7905f395cce4062e06c403ad
Niklas Mähler <niklas.mahler@nmbu.no>
2014-09-26 11:33:24 +0200

    improved styling of documentation page
    The styling has been improved so that sections are clearer, and that
    should make it easier to read and to find stuff on the page.

--- application/controllers/Documentation.php
+++ application/controllers/Documentation.php
@@ -3,7 +3,11 @@
 class Documentation extends MY_Controller {
 
 	public function index() {
-		$this->load->view('base/header', $this->get_head_data('documentation', 'Documentation'));
+		$this->load->view('base/header',
+			$this->get_head_data('documentation', 'Documentation',
+				array(base_url(array('assets', 'css', 'documentation.css')))
+			)
+		);
 		$this->load->view('documentation_view');
 		$this->load->view('base/footer', $this->get_foot_data());
 	}
